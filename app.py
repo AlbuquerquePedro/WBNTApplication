35fa6ca5bad7ca78ff5d56711066f5150836aa54
AlbuquerquePedro <101055754+AlbuquerquePedro@users.noreply.github.com>
2024-06-02 13:23:52 +0300

    change variable

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+web_app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///mydatabase.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+database_url_default = 'sqlite:///default.db'
+web_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', database_url_default)
+web_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+database = SQLAlchemy(web_app)
 
 from routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    web_app.run(debug=True, port=5000)
