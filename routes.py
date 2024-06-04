a60eb613b314afe69a78101240e60f252ecb608a
AlbuquerquePedro <101055754+AlbuquerquePedro@users.noreply.github.com>
2024-06-05 01:39:28 +0300

    improve variables

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from controllers import get_all_notes, get_note, create_note, update_note, delete_note
+from controllers import fetch_all_notes, fetch_note_by_id, create_new_note, update_existing_note, remove_note
 import os
 from dotenv import load_dotenv
 
@@ -8,30 +8,30 @@ load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():
+def homepage():
     return "<h1>Welcome to the Notes App</h1>"
 
 @app.route('/api/notes', methods=['GET'])
-def api_get_all_notes():
-    return jsonify(get_all_notes())
+def handle_get_all_notes():
+    return jsonify(fetch_all_notes())
 
 @app.route('/api/notes/<int:note_id>', methods=['GET'])
-def api_get_note(note_id):
-    return jsonify(get_note(note_id))
+def handle_get_single_note(note_id):
+    return jsonify(fetch_note_by_id(note_id))
 
 @app.route('/api/notes', methods=['POST'])
-def api_create_note():
-    note_data = request.json
-    return jsonify(create_note(note_data)), 201
+def handle_create_note():
+    note_details = request.json
+    return jsonify(create_new_note(note_details)), 201
 
 @app.route('/api/notes/<int:note_id>', methods=['PUT'])
-def api_update_note(note_id):
-    note |data = request.json
-    return jsonify(update_note(note_id, note_data))
+def handle_update_note(note_id):
+    updated_note_details = request.json
+    return jsonify(update_existing_note(note_id, updated_note_details))
 
 @app.route('/api/notes/<int:note_id>', methods=['DELETE'])
-def api_delete_note(note_id):
-    return jsonify(delete_note(note_id))
+def handle_delete_note(note_id):
+    return jsonify(remove_note(note_id))
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv('PORT', 5000))
